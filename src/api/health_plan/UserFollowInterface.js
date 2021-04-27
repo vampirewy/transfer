65c2df31d2f283f8cc72ca32270bc6644d7524a8
csx <839306801@qq.com>
2021-04-27 19:37:35 +0800

    openTable

--- src/api/health_plan/UserFollowInterface.js
+++ src/api/health_plan/UserFollowInterface.js
@@ -50,8 +50,12 @@ class UserFollowInterface extends BaseModule {
   }
 
   // 用户随访列表
-  getIntervenePlanPageList(data = {}) {
+  /* getIntervenePlanPageList(data = {}) {
     return this.post('/intervene_plan/get_intervene_plan_page_list', data);
+  } */
+  // 用户创建计划 - 随访列表lvb copy
+  getIntervenePlanPageList(data = {}) {
+    return this.post('/client_info/get_client_info_create_plan_list_page', data);
   }
   // 待随访计划 / 随访记录
   getIntervenePlanListPage(data = {}) {
