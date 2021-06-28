718458fc55e1949732dc18b19246f16032614184
csx <839306801@qq.com>
2021-06-28 17:40:05 +0800

    个人医疗信息公用

--- src/components/user_health/lifestyle.vue
+++ src/components/user_health/lifestyle.vue
@@ -3,19 +3,19 @@
     <div class="life-style">
       <p class="nameP">
         <img src="@/assets/images/healthPlan/lifestyle1.png"/>饮食：
-        <span>{{formNew.questionLifestyle1.paramValue || '您的膳食结构基本合理'}}</span>
+        <span>{{formNew.questionLifestyle1.paramValue || '暂无数据'}}</span>
       </p>
       <p class="nameP"><img src="@/assets/images/healthPlan/lifestyle2.png"/>吸烟：
         <span>{{formNew.questionLifestyle2.paramValue || '暂无数据'}}</span>
       </p>
       <p class="nameP"><img src="@/assets/images/healthPlan/lifestyle3.png"/>饮酒：
-        <span style="color:#F33D21;">{{formNew.questionLifestyle3.paramValue || '您会酗酒'}}</span>
+        <span style="color:#F33D21;">{{formNew.questionLifestyle3.paramValue || '暂无数据'}}</span>
       </p>
       <p class="nameP"><img src="@/assets/images/healthPlan/lifestyle4.png"/>精神：
-        <span style="color:#F33D21;">{{formNew.questionLifestyle4.paramValue || '您的精神压力有点大'}}</span>
+        <span style="color:#F33D21;">{{formNew.questionLifestyle4.paramValue || '暂无数据'}}</span>
       </p>
       <p class="nameP"><img src="@/assets/images/healthPlan/lifestyle5.png"/>睡眠：
-        <span>{{formNew.questionLifestyle5.paramValue || '您的睡眠状况一般'}}</span>
+        <span>{{formNew.questionLifestyle5.paramValue || '暂无数据'}}</span>
       </p>
     </div>
   </div>
@@ -24,7 +24,7 @@
 <script>
 export default {
   name: 'disease',
-  props: ['clientId'],
+  props: ['questionLifestyle'],
   data() {
     return {
       formNew: {
@@ -38,25 +38,11 @@ export default {
     };
   },
   watch: {
-    // questionLifestyle: {
-    //   handler(val) {
-    //     this.formNew = val;
-    //   },
-    //   deep: true, // 为true，表示深度监听，这时候就能监测到a值变化
-    // },
-  },
-  mounted() {
-    this.getImportantIndex();
-  },
-  methods: {
-    getImportantIndex() {
-      this.$api.health
-        .getabnormalList(this.clientId)
-        .then((res) => {
-          const { data } = res;
-          const result = data.data;
-          this.assessListData = result || [];
-        });
+    questionLifestyle: {
+      handler(val) {
+        this.formNew = val;
+      },
+      deep: true, // 为true，表示深度监听，这时候就能监测到a值变化
     },
   },
 };
