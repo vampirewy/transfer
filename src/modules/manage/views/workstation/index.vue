e522d31f345fd75afcc75bd2e044ca797ca00ec3
wangyi <haveaballmyball@hotmail.com>
2021-07-14 16:58:51 +0800

    feat:添加工作台路由及页面

---
+++ src/modules/manage/views/workstation/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    正在开发中
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'workstation',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
