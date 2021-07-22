20087d383f9810d1deabe75a8270f77b987878fc
wangyi <haveaballmyball@hotmail.com>
2021-07-22 10:19:34 +0800

    feat:添加首次手动上报弹窗及mock

--- src/components/track_pop/index.vue
+++ src/components/track_pop/index.vue
@@ -1,16 +1,17 @@
 <template>
-   <!-- 跟踪结果弹窗 -->
+  <!-- 跟踪结果弹窗 -->
   <el-dialog
-    title="跟踪结果" :visible.sync="visible"
+    title="跟踪结果"
+    :visible.sync="isShowTrackPop"
     class="dialog-detail"
     :modal-append-to-body="false"
     width="600px"
     @close="cancel"
   >
     <div>
-      <el-form :model="form1" label-width="80px">
+      <el-form :model="form" label-width="100px" :rules="rules">
         <el-form-item label="跟踪结果：" prop="result">
-          <el-radio-group v-model="form1.checkRadio">
+          <el-radio-group v-model="form.result">
             <el-radio :label="1">已回访</el-radio>
             <el-radio :label="2">拒接</el-radio>
             <el-radio :label="3">关机</el-radio>
@@ -18,39 +19,64 @@
             <el-radio :label="5">号码错误</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="跟踪记录：" prop="remark">
-          <el-input type="textarea" placeholder="请输入" v-model="form1.remark"></el-input>
+        <el-form-item label="跟踪记录：" prop="trackRecord">
+          <el-input
+            type="textarea"
+            placeholder="请输入"
+            v-model="form.trackRecord"
+            maxlength="300"
+          ></el-input>
         </el-form-item>
       </el-form>
     </div>
     <div slot="footer" class="dialog-footer">
       <el-button size="small" @click="cancel" class="cancelBtn">取消</el-button>
-      <el-button type="primary" size="small" @click="onDialogConfirm" class="sureBtn">确定</el-button>
+      <el-button
+        type="primary"
+        size="small"
+        @click="onDialogConfirm"
+        class="sureBtn"
+        >确定</el-button
+      >
     </div>
   </el-dialog>
 </template>
 <script>
 export default {
   props: {
-    visible: Boolean,
-    clientId: String,
+    isShowTrackPop: Boolean,
+    trackingId: String,
   },
   data() {
     return {
-      form1: {
-        checkRadio: '',
-        reasonRadio: '',
-        remark: '',
+      rules: {
+        result: [{ required: true }],
+      },
+      form: {
+        result: 1,
+        trackRecord: '',
       },
     };
   },
   methods: {
-    onDialogConfirm() {
-      console.log('跟踪结果弹窗关闭！');
-      this.$emit('change', '关闭');
+    async onDialogConfirm() {
+      const isSuccess = await this.getTrankingRequest();
+      if (isSuccess) {
+        this.$message.success('操作成功');
+      }
+      this.$emit('cancel');
     },
     cancel() {
       this.$emit('cancel');
+    },
+    async getTrankingRequest() {
+      const params = {
+        trackingId: this.trackingId,
+        ...this.form,
+      };
+      const res = await this.$api.sunFollow.saveTranking(params);
+      const { success } = res.data;
+      if (success) return success;
     },
   },
 };
@@ -66,7 +92,4 @@ export default {
     padding: 0 32px;
   }
 }
-
-
 </style>
-
